b23b9e33cabcbccd482de30237ac8d9e9965043a
Navy <navydotgif@gmail.com>
2019-08-01 02:01:25 +0300

    Built in embeds in responses

--- discordbot/src/main/java/com/github/navy/discordbot/structures/Response.java
+++ discordbot/src/main/java/com/github/navy/discordbot/structures/Response.java
@@ -1,9 +1,14 @@
 package com.github.navy.discordbot.structures;
 
+import java.awt.Color;
+
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
 public class Response {
 
-	String text;
-	boolean embed = false;
+	public String text;
+	public boolean em = false;
+	public EmbedBuilder embed = null;
 	
 	public Response(String text) {
 		
@@ -14,6 +19,24 @@ public class Response {
 	public Response(String text, boolean embed) {
 		
 		this.text = text;
+		this.em = embed;
+		this.embed = new EmbedBuilder();
+		this.embed.setColor(Color.GREEN);
+		this.embed.setDescription(text);
+		
+	}
+	
+	public Response(String text, EmbedBuilder embed) {
+		
+		this.text = text;
+		this.em = true;
+		this.embed = embed;
+		
+	}
+	
+	public Response(EmbedBuilder embed) {
+		
+		this.em = true;
 		this.embed = embed;
 		
 	}
