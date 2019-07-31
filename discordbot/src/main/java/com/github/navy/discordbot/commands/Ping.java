e08c67d0a655b509ea889f6736abfd4df040d8a5
Navy <navydotgif@gmail.com>
2019-08-01 02:36:56 +0300

    proper ping command

--- discordbot/src/main/java/com/github/navy/discordbot/commands/Ping.java
+++ discordbot/src/main/java/com/github/navy/discordbot/commands/Ping.java
@@ -1,9 +1,12 @@
 package com.github.navy.discordbot.commands;
 
+import java.util.Date;
+
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.server.Server;
 
+import com.github.navy.discordbot.framework.Client;
 import com.github.navy.discordbot.structures.Command;
 import com.github.navy.discordbot.structures.CommandInterface;
 import com.github.navy.discordbot.structures.Response;
@@ -16,9 +19,16 @@ public class Ping extends Command implements CommandInterface {
 		
 	}
 	
-	public Response call(Message message, String[] args, TextChannel channel, Server guild) {
+	public Response call(Message message, String[] args, TextChannel channel, Server guild, Client client) {
 		
-		return new Response("Pong!");
+		if(!channel.canYouWrite()) return null;
+		
+		long now = (new Date()).getTime();
+		Message msg = channel.sendMessage("Pinging...").join();
+		long end = (new Date()).getTime();
+		msg.edit("**Pong!**\nTook " + (end-now) + "ms to complete.");
+		
+		return null;
 		
 	}
 
